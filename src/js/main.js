345c845ec00216dabf85c317869317283a1c5c9c
Crokuz <leander.norberg@hotmail.com>
2026-02-20 22:03:13 +0100

    Added content filtering function

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,32 @@
+let allCourses = [];
+
 document.addEventListener("DOMContentLoaded", async () => {
-    getCourses();   
+    await getCourses();  
+    document.querySelector("#search").addEventListener("input", filterCourses);
 });
+
+function filterCourses(input) {
+    const searchPhrase = input.target.value.toLowerCase();
+
+    const filtered = allCourses.filter(course =>
+        course.code.toLowerCase().includes(searchPhrase) ||
+        course.coursename.toLowerCase().includes(searchPhrase) ||
+        course.progression.toLowerCase().includes(searchPhrase)
+    );
+
+    displayData(filtered);
+}
 
 async function getCourses() {
     const url ="https://webbutveckling.miun.se/files/ramschema.json";
 
     try {
         const response = await fetch(url);
-
         const courses = await response.json();
-        console.log(courses);
-        displayData(courses);
+
+        allCourses = courses;
+        displayData(allCourses);
+
     } catch (error) {
         console.error("Error: " + error);
     }
